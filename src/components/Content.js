5378cacc833b7198bea2cea474b0e998b073891a
Chad <49378047+chadracelis@users.noreply.github.com>
2019-08-03 19:37:55 -0400

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { exchangeSelector } from '../store/selectors'
-import { loadAllOrders, subscribeToEvents } from '../store/interactions'
-import OrderBook from './OrderBook'
-import Trades from './Trades'
-import MyTransactions from './MyTransactions'
-import PriceChart from './PriceChart'
-import Balance from './Balance'
-import NewOrder from './NewOrder'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { exchangeSelector } from '../store/selectors';
+import { loadAllOrders, subscribeToEvents } from '../store/interactions';
+import Trades from './Trades';
+import OrderBook from './OrderBook';
+import MyTransactions from './MyTransactions';
+import PriceChart from './PriceChart';
+import Balance from './Balance';
+import NewOrder from './NewOrder';
 
-class Content extends Component {
+export class Content extends Component {
+
   componentWillMount() {
     this.loadBlockchainData(this.props)
   }
@@ -45,3 +46,5 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Content)
+
+
