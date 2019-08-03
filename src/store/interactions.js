0162ab1313176f72ac271a42ea897c3b935a8311
Chad <49378047+chadracelis@users.noreply.github.com>
2019-08-03 19:44:26 -0400

    Update interactions.js

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,4 +1,4 @@
-import Web3 from 'web3'
+import Web3 from 'web3';
 import {
   web3Loaded,
   web3AccountLoaded,
@@ -20,99 +20,77 @@ import {
   buyOrderMaking,
   sellOrderMaking,
   orderMade
-} from './actions'
-import Token from '../abis/Token.json'
-import Exchange from '../abis/Exchange.json'
+} from './actions';
+import Token from '../abis/Token.json';
+import Exchange from '../abis/Exchange.json';
 import { ETHER_ADDRESS } from '../helpers'
 
 export const loadWeb3 = (dispatch) => {
-  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
-  dispatch(web3Loaded(web3))
-  return web3
+  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+  dispatch(web3Loaded(web3));
+  return web3;
 }
 
 export const loadAccount = async (web3, dispatch) => {
-  const accounts = await web3.eth.getAccounts()
-  const account = accounts[0]
-  dispatch(web3AccountLoaded(account))
-  return account
+  const accounts = await web3.eth.getAccounts();
+  const account = accounts[0];
+  dispatch(web3AccountLoaded(account));
+  return account;
 }
 
 export const loadToken = async (web3, networkId, dispatch) => {
   try {
-    const token = web3.eth.Contract(Token.abi, Token.networks[networkId].address)
-    dispatch(tokenLoaded(token))
-    return token
-  } catch (error) {
-    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
+    const token = web3.eth.Contract(Token.abi, Token.networks[networkId].address);
+    dispatch(tokenLoaded(token));
+    return token;
+  } catch(error) {
+    window.alert('Contract not deployed to the current network. Please select another network with Metamask.')
     return null
   }
 }
 
 export const loadExchange = async (web3, networkId, dispatch) => {
   try {
-    const exchange = web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address)
-    dispatch(exchangeLoaded(exchange))
-    return exchange
-  } catch (error) {
-    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
+    const exchange = web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address);
+    dispatch(exchangeLoaded(exchange));
+    return exchange;
+  } catch(error) {
+    window.alert('Contract not deployed to the current network. Please select another network with Metamask.')
     return null
   }
 }
 
 export const loadAllOrders = async (exchange, dispatch) => {
-  // Fetch cancelled orders with the "Cancel" event stream
-  const cancelStream = await exchange.getPastEvents('Cancel', { fromBlock: 0, toBlock: 'latest' })
+  // Fetch cancelled orders with the "Cancel" event
+  const cancelStream = await exchange.getPastEvents('Cancel', { fromBlock: 0, toBlock: 'latest'})
   // Format cancelled orders
   const cancelledOrders = cancelStream.map((event) => event.returnValues)
   // Add cancelled orders to the redux store
   dispatch(cancelledOrdersLoaded(cancelledOrders))
 
-  // Fetch filled orders with the "Trade" event stream
-  const tradeStream = await exchange.getPastEvents('Trade', { fromBlock: 0, toBlock: 'latest' })
+  // Fetch filled orders with the "Trade" event
+  const tradeStream = await exchange.getPastEvents('Trade', { fromBlock: 0, toBlock: 'latest'})
   // Format filled orders
   const filledOrders = tradeStream.map((event) => event.returnValues)
-  // Add cancelled orders to the redux store
+  // Add filled orders to the redux store
   dispatch(filledOrdersLoaded(filledOrders))
 
-  // Load order stream
-  const orderStream = await exchange.getPastEvents('Order', { fromBlock: 0,  toBlock: 'latest' })
+  // Fetch all orders with the "Order" event
+  const orderStream = await exchange.getPastEvents('Order', { fromBlock: 0, toBlock: 'latest'})
   // Format order stream
   const allOrders = orderStream.map((event) => event.returnValues)
   // Add open orders to the redux store
   dispatch(allOrdersLoaded(allOrders))
 }
 
-export const subscribeToEvents = async (exchange, dispatch) => {
-  exchange.events.Cancel({}, (error, event) => {
-    dispatch(orderCancelled(event.returnValues))
-  })
-
-  exchange.events.Trade({}, (error, event) => {
-    dispatch(orderFilled(event.returnValues))
-  })
-
-  exchange.events.Deposit({}, (error, event) => {
-    dispatch(balancesLoaded())
-  })
-
-  exchange.events.Withdraw({}, (error, event) => {
-    dispatch(balancesLoaded())
-  })
-
-  exchange.events.Order({}, (error, event) => {
-    dispatch(orderMade(event.returnValues))
-  })
-}
-
 export const cancelOrder = (dispatch, exchange, order, account) => {
   exchange.methods.cancelOrder(order.id).send({ from: account })
   .on('transactionHash', (hash) => {
-     dispatch(orderCancelling())
+    dispatch(orderCancelling())
   })
   .on('error', (error) => {
     console.log(error)
-    window.alert('There was an error!')
+    window.alert('There was an error with cancelling order')
   })
 }
 
@@ -123,7 +101,7 @@ export const fillOrder = (dispatch, exchange, order, account) => {
   })
   .on('error', (error) => {
     console.log(error)
-    window.alert('There was an error!')
+    window.alert('There was an error with filling order')
   })
 }
 
@@ -148,14 +126,17 @@ export const loadBalances = async (dispatch, web3, exchange, token, account) => 
   dispatch(balancesLoaded())
 }
 
+/* NOTES TO SELF */
+// Dispatch is a function that gets called into the reducer to handle the action
+
 export const depositEther = (dispatch, exchange, web3, amount, account) => {
-  exchange.methods.depositEther.send({ from: account,  value: web3.utils.toWei(amount, 'ether') })
+  exchange.methods.depositEther.send({ from: account, value: web3.utils.toWei(amount, 'ether') })
   .on('transactionHash', (hash) => {
     dispatch(balancesLoading())
   })
   .on('error',(error) => {
     console.error(error)
-    window.alert(`There was an error!`)
+    window.alert(`There was an error with depositing`)
   })
 }
 
@@ -166,7 +147,7 @@ export const withdrawEther = (dispatch, exchange, web3, amount, account) => {
   })
   .on('error',(error) => {
     console.error(error)
-    window.alert(`There was an error!`)
+    window.alert(`There was an error with withdrawing`)
   })
 }
 
@@ -181,7 +162,7 @@ export const depositToken = (dispatch, exchange, web3, token, amount, account) =
     })
     .on('error',(error) => {
       console.error(error)
-      window.alert(`There was an error!`)
+      window.alert(`There was an error with depositing`)
     })
   })
 }
@@ -193,7 +174,7 @@ export const withdrawToken = (dispatch, exchange, web3, token, amount, account) 
   })
   .on('error',(error) => {
     console.error(error)
-    window.alert(`There was an error!`)
+    window.alert(`There was an error with withdrawing`)
   })
 }
 
@@ -209,7 +190,7 @@ export const makeBuyOrder = (dispatch, exchange, token, web3, order, account) =>
   })
   .on('error',(error) => {
     console.error(error)
-    window.alert(`There was an error!`)
+    window.alert(`There was an error with buying order`)
   })
 }
 
@@ -225,6 +206,29 @@ export const makeSellOrder = (dispatch, exchange, token, web3, order, account) =
   })
   .on('error',(error) => {
     console.error(error)
-    window.alert(`There was an error!`)
+    window.alert(`There was an error with selling order`)
+  })
+}
+
+// Rerender after interaction
+export const subscribeToEvents = async (exchange, dispatch) => {
+  exchange.events.Cancel({}, (error, event) => {
+    dispatch(orderCancelled(event.returnValues))
+  })
+
+  exchange.events.Trade({}, (error, event) => {
+    dispatch(orderFilled(event.returnValues))
+  })
+
+  exchange.events.Deposit({}, (error, event) => {
+    dispatch(balancesLoaded())
+  })
+
+  exchange.events.Withdraw({}, (error, event) => {
+    dispatch(balancesLoaded())
+  })
+
+  exchange.events.Order({}, (error, event) => {
+    dispatch(orderMade(event.returnValues))
   })
 }
