8580e54740cc64cbd8582b8aa8a7ca39a122d6ee
frank <frank.x@aliyun.com>
2020-11-25 22:16:17 +0800

    for web3-v1.3.0

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -26,7 +26,7 @@ import Exchange from '../abis/Exchange.json';
 import { ETHER_ADDRESS } from '../helpers'
 
 export const loadWeb3 = (dispatch) => {
-  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+  const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
   dispatch(web3Loaded(web3));
   return web3;
 }
@@ -40,7 +40,7 @@ export const loadAccount = async (web3, dispatch) => {
 
 export const loadToken = async (web3, networkId, dispatch) => {
   try {
-    const token = web3.eth.Contract(Token.abi, Token.networks[networkId].address);
+    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address);
     dispatch(tokenLoaded(token));
     return token;
   } catch(error) {
@@ -51,7 +51,7 @@ export const loadToken = async (web3, networkId, dispatch) => {
 
 export const loadExchange = async (web3, networkId, dispatch) => {
   try {
-    const exchange = web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address);
+    const exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address);
     dispatch(exchangeLoaded(exchange));
     return exchange;
   } catch(error) {
